bb677d7fdf11837bc6b7604e82a75e651638065a
Andres <andres.lazaro.nu@gmail.com>
2025-10-04 16:25:31 +0200

    feat(3d_components)

---
+++ src/components/3d/index.ts
@@ -0,0 +1,4 @@
+// 3D Components Barrel Exports
+export { Asteroid3D } from './Asteroid3D';
+export { default as Earth3DGlobe } from './Earth3DGlobe';
+export { AnimatedBackground } from './AnimatedBackground';
